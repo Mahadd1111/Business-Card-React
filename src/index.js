587a8399d576667102d26211f4476c2db4360599
Mahad Ahmed <mahadddd1111@gmail.com>
2022-08-21 12:36:21 +0500

    Initial Commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import profilePic from "./imgs/profile.jpg";
+import "./styles.css"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function Profile(){
+    return(
+        <div id="profile">
+            <img src={profilePic} className="profile-pic"></img>
+        </div>
+    );
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+function About(){
+    return(
+        <div className="about">
+            <h1>Laura Smith</h1>
+            <h3>Front end Developer</h3>
+            <small>LauraSmith.website</small>
+            <div className="contact">
+                <button className="btn btn-lg btn-light"><i className="fa-solid fa-envelope"></i> Email</button>
+                <button className="btn btn-lg btn-primary"><i className="fa-brands fa-linkedin"></i> LinkedIn</button>
+            </div>
+        </div>
+    );
+}
+
+function Information(){
+    return(
+        <div className="info">
+            <h1>About</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et porta ante. Vestibulum eu mollis elit, et mattis felis. Donec lacinia nisl ac dolor imperdiet, vel venenatis justo ullamcorper. Phasellus convallis tempor odio, at sodales ex ultrices molestie.</p>
+            <h1>Interests</h1>
+            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec et porta ante. Vestibulum eu mollis elit, et mattis felis. Donec lacinia nisl ac dolor imperdiet, vel venenatis justo ullamcorper. Phasellus convallis tempor odio, at sodales ex ultrices molestie.</p>
+        </div>
+    );
+}
+
+function Footer(){
+    return(
+        <div className="footer">
+            <i class="fa-brands fa-facebook"></i>
+            <i class="fa-brands fa-twitter"></i>
+            <i class="fa-brands fa-instagram"></i>
+            <i class="fa-brands fa-github"></i>
+        </div>
+    );
+}
+
+function BusinessCard(){
+    return(
+        <div>
+            <Profile/>
+            <About/>
+            <Information/>
+            <Footer/>
+        </div>
+        
+    );
+}
+
+const root=ReactDOM.createRoot(document.querySelector("#root"));
+root.render(<BusinessCard/>);
